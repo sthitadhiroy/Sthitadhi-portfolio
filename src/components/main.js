2a4de45370574e7d1752d3d29bc55a6fd124feff
Sthitadhi Roy <sthitadhiroy@Sthitadhis-MacBook-Air.local>
2020-04-26 20:58:39 +0530

    deploy02

--- src/components/main.js
+++ src/components/main.js
@@ -9,11 +9,11 @@ import Resume from "./resume";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
+    <Route path="/" component={LandingPage} />
   </Switch>
 );
 
